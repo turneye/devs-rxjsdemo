71132907f327bf4ca1c26b70eaf6872f9c78ab81
Marcus Turncode <marcus@turncode.productions>
2018-09-05 17:10:33 +1000

    work

---
+++ ClientApp/src/app/basic/basic.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { EverythingService, ToDoItem, User } from '../everything.service';
+
+import { Observable, Subject, of } from 'rxjs';
+import { filter, reduce, switchMap, tap, take, mergeMap, mergeAll, toArray, map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-basic',
+  templateUrl: './basic.component.html',
+  styleUrls: ['./basic.component.css']
+})
+export class BasicComponent implements OnInit {
+  items$: Observable<ToDoItem[]>;
+  totals$: Observable<Totals>;
+  total: Totals = { total: 0, complete: 0};
+
+  searchTerm$ = new Subject<string>();
+  users$: Observable<User[]>;
+
+  constructor(public service: EverythingService) {}
+
+  ngOnInit() {
+    this.items$ = this.service.getItems();
+    this.items$.subscribe(val => this.totals$ = of(val).pipe(mergeAll(), reduce((tot: Totals, itm: ToDoItem) => {
+      return {
+        total: tot.total += 1,
+        complete: tot.complete += (itm.done ? 1 : 0)
+      };
+    }, { total: 0, complete: 0 }), tap(info => console.log(info))));
+
+    // 1 - basic
+    // this.users$ = this.service.getUsers();
+
+    // 2 - just give me the top 3. take
+    // this.users$ = this.service.getUsers().pipe(take(3));  // hmmm ..... why doesn't this work??
+    // this.users$ = this.service.getUsers().pipe(mergeAll(), take(3), toArray()); // .subscribe(val => console.log('Next: ' + val.name));
+
+    // 3 - searching with debounce, etc
+    // this.users$ = this.service.searchUsers(this.searchTerm$);
+  }
+
+  totalItems() {}
+  itemsComplete() {}
+
+  newItem() {
+    this.service.addItem('Item', 'Description');
+  }
+
+  // calcTotal() {
+  //   this.items$
+  //     .pipe(
+  //       switchMap(data => data as ToDoItem[]),
+  //       reduce((data: Totals, item: ToDoItem) => {
+  //         data.complete += item.done ? 1 : 0;
+  //         data.total += 1;
+  //         console.log(data);
+  //         return data;
+  //       }, { total: 0, complete: 0})
+  //     )
+  //     .subscribe(data => {
+  //       console.log(data);
+  //       this.totals = data;
+  //     });
+
+  // }
+
+  toggleItem(item: ToDoItem) {
+    item.done = !item.done;
+    this.service.updateItem(item);
+  }
+}
+
+export class Totals {
+  public total: number;
+  public complete: number;
+}
---
+++ ClientApp/src/app/everything.service.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, BehaviorSubject } from 'rxjs';
+import {
+  tap,
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+  filter,
+  map
+} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EverythingService {
+  private _items: ToDoItem[] = [];
+  private itemsAsObservable = of(this._items); // NOTE : Observable.of has just become of in rxjs 6
+  private _obsItems: BehaviorSubject<ToDoItem[]> = new BehaviorSubject(
+    this._items
+  );
+
+  constructor(public http: HttpClient) {}
+
+  private id = 1;
+
+  addItem(item: string, desc: string) {
+    this._items.push({ title: item, description: desc, done: false, id: this.id });
+    this.id++;
+    this._obsItems.next(this._items);
+  }
+  updateItem(item: ToDoItem) {
+    this._items.find(x => x.id === item.id).done = item.done;
+    this._obsItems.next(this._items);
+  }
+
+  getItems() {
+    return this._obsItems;
+  }
+
+  getUsers() {
+    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users');
+    // .pipe(tap(x => console.log(x)));
+  }
+
+  searchUsers(search: Observable<string>) {
+    return search.pipe(
+      switchMap(term =>
+        this.getUsers().pipe(
+          map(usr => usr.filter(x => x.name.indexOf(term) >= 0))
+        )
+      )
+    );
+
+    // WITH DEBOUNCE, DISTINCT
+    // return search.pipe(
+    //         debounceTime(500),
+    //         distinctUntilChanged(),
+    //         tap(val => console.log('searching for value : ' + val)),
+    //         switchMap(term => this.getUsers().pipe(map(usr => usr.filter(x => x.name.indexOf(term) >= 0)))));
+  }
+}
+
+export class User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+export class Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+export class Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export class Geo {
+  lat: number;
+  lng: number;
+}
+export class ToDoItem {
+  public title: string;
+  public description: string;
+  public done: boolean;
+  public id: number;
+}
